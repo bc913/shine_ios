805a7485dc70da1d231d6d70968534d9de18c892
Burak Can <chuckthecode@gmail.com>
2017-10-17 07:21:02 -0400

    TA40001 - update table view for dance types

---
+++ OneDance/ViewControllers/ProfileSetup/DanceTypesViewController.swift
@@ -0,0 +1,147 @@
+//
+//  DanceTypesViewController.swift
+//  OneDance
+//
+//  Created by Burak Can on 10/15/17.
+//  Copyright © 2017 Burak Can. All rights reserved.
+//
+
+import UIKit
+
+class DanceTypesViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var doneButton: UIButton!
+    
+    var viewModel : DanceTypesViewModel?{
+        willSet{
+            viewModel?.viewDelegate = nil
+        }
+        didSet{
+            viewModel?.viewDelegate = self
+            refreshDisplay()
+        }
+    }
+    
+    var isLoaded: Bool = false
+    var isAnyRowSelected = false
+    
+    func refreshDisplay()
+    {
+        if let viewModel = self.viewModel , isLoaded {
+            
+            self.title = "Select your dances"
+        } else {
+            self.title = ""
+        }
+        self.tableView?.reloadData()
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Configure button
+        self.doneButton.configure(title: "Done", backgroundColor: UIColor(red:0.23, green:0.35, blue:0.60, alpha:1.0))
+
+        // Configure navigation bar
+        // TODO: Update this code
+        let navigationTitleFont = UIFont(name: "Avenir", size: 20)!
+        self.navigationController?.navigationBar.titleTextAttributes = [NSFontAttributeName: navigationTitleFont, NSForegroundColorAttributeName : UIColor.white]
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.backgroundColor = UIColor(red: 0, green: 0.17, blue: 0.21, alpha: 1.0)
+        
+        // Hide empty unused cells
+        self.tableView.tableFooterView = UIView()
+        
+        // Register custom view cell
+        tableView.register(UINib(nibName:"DanceTypeTableViewCell", bundle:nil), forCellReuseIdentifier: "DanceTypeCell")
+        
+        // Multiple selection
+        self.tableView.allowsMultipleSelection = true
+        
+        self.isLoaded = true
+        refreshDisplay()
+    }
+
+    
+    
+}
+
+extension DanceTypesViewController : UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let viewModel = self.viewModel {
+            return viewModel.numberOfItems
+        }
+        
+        return 0
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DanceTypeCell", for: indexPath) as! DanceTypeTableViewCell
+        cell.item = self.viewModel?.itemAtIndex(indexPath.row)
+        //cell.selectionStyle = .none
+        
+        return cell
+    }
+}
+
+extension DanceTypesViewController : UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let selectedCell = self.tableView.cellForRow(at: indexPath) as? DanceTypeTableViewCell{
+            selectedCell.layer.borderColor = UIColor.blue.cgColor
+            selectedCell.accessoryType = .checkmark
+            print("osman selected")
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let selectedCell = self.tableView.cellForRow(at: indexPath) as? DanceTypeTableViewCell {
+            selectedCell.layer.borderColor = selectedCell.defaultBorderColor
+            selectedCell.accessoryType = .none
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
+    
+    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+}
+
+extension DanceTypesViewController : DanceTypesViewModelViewDelegate {
+    
+    func itemsDidChange(viewModel: DanceTypesViewModelType) {
+        self.tableView.reloadData()
+    }
+    
+    func notifyUser(_ viewModel: DanceTypesViewModelType, _ title: String, _ message: String) {
+        // create the alert
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        // add the actions (buttons)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+        return
+    }
+}
--- OneDance/ViewControllers/TableCells/DanceTypeTableViewCell.swift
+++ OneDance/ViewControllers/TableCells/DanceTypeTableViewCell.swift
@@ -22,17 +22,33 @@ class DanceTypeTableViewCell: UITableViewCell {
         }
     }
     
+    var defaultBorderColor : CGColor?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         self.contentView.backgroundColor = UIColor(red: 0, green: 0.17, blue: 0.21, alpha: 1.0)
+        // For accessory view background color
+        self.contentView.superview?.backgroundColor = UIColor(red: 0, green: 0.17, blue: 0.21, alpha: 1.0)
+        self.accessoryView?.backgroundColor = UIColor(red: 0, green: 0.17, blue: 0.21, alpha: 1.0)
         self.danceTypeLabel.textColor = UIColor.white
+        
+        self.defaultBorderColor = self.layer.borderColor
+        
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
+        
+        // Update the background colors to hide highlighting
+        self.contentView.backgroundColor = UIColor(red: 0, green: 0.17, blue: 0.21, alpha: 1.0)
+        // For accessory view background color
+        self.contentView.superview?.backgroundColor = UIColor(red: 0, green: 0.17, blue: 0.21, alpha: 1.0)
+        self.backgroundColor = UIColor(red: 0, green: 0.17, blue: 0.21, alpha: 1.0)
+        self.accessoryView?.backgroundColor = UIColor(red: 0, green: 0.17, blue: 0.21, alpha: 1.0)
+        
     }
     
 }
